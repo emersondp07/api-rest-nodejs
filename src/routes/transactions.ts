58791d592f109450f1d236a0bb93380a67509331
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2023-11-02 20:10:16 -0300

    middleware sessionId validation all routes and create first unit-test

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,25 +3,74 @@ import { FastifyInstance } from "fastify";
 
 import { z } from "zod";
 import { knex } from "../database";
+import { checkSessionIdExists } from "../middleware/check-session-id-exists";
+
+// Cookies <--> Formas da gente manter context entre requisições
+
+// Unitarios: unidade de sua aplicação
+// Integração: Comunicação entre duas ou mais unidades
+// e2e: ponta a ponta: simula um usuário operando na nossa aplicação
+
+// front-end: abre a página de login, digite o texto emerson.dantaspereira@hotmail.com no campo com10 email, clique no botão
+// back-end: chamadas HTTP, WebSockets
+
+// Pirâmide de testes: E2E (não depender de nenhuma tecnologia, não dependem de arquitetura)
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const transactions = await knex("transactions").select();
+  app.get(
+    "/",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies;
 
-    return { transactions };
-  });
+      const transactions = await knex("transactions")
+        .where("session_id", sessionId)
+        .select();
 
-  app.get("/:id", async (request) => {
-    const getTransactionParamsSchema = z.object({
-      id: z.string().uuid(),
-    });
+      return { transactions };
+    }
+  );
 
-    const { id } = getTransactionParamsSchema.parse(request.params);
+  app.get(
+    "/:id",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const getTransactionParamsSchema = z.object({
+        id: z.string().uuid(),
+      });
 
-    const transaction = await knex("transactions").where("id", id).first();
+      const { id } = getTransactionParamsSchema.parse(request.params);
 
-    return { transaction };
-  });
+      const { sessionId } = request.cookies;
+
+      const transaction = await knex("transactions")
+        .where({ session_id: sessionId, id })
+        .first();
+
+      return { transaction };
+    }
+  );
+
+  app.get(
+    "/summary",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
+
+      const summary = await knex("transactions")
+        .where("session_id", sessionId)
+        .sum("ammount", { as: "ammount" })
+        .first();
+
+      return { summary };
+    }
+  );
 
   app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
@@ -34,10 +83,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body
     );
 
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
     await knex("transactions").insert({
       id: randomUUID(),
       title,
       ammount: type === "credit" ? ammount : ammount * -1,
+      session_id: sessionId,
     });
 
     return reply.status(201).send();
